bf4e893ab50c97a2a362d897f75289250495d88a
Mark Lai <marklai@hk01.com>
2020-05-05 02:51:53 +0800

    feat: add 1.14 map

--- src/components/Map/Selector/index.js
+++ src/components/Map/Selector/index.js
@@ -1,78 +1,84 @@
-import React from 'react'
+import React from "react";
 
-import './index.css'
+import "./index.css";
 
 export class Selector extends React.PureComponent {
   state = {
-    show: false
-  }
+    show: false,
+  };
 
   toggleNav = () => {
     this.setState({
-      show: !this.state.show
-    })
-  }
+      show: !this.state.show,
+    });
+  };
 
-  onChangeMap = mapId => {
-    this.props.setMapId(mapId)
-    this.setState({ show: false })
-  }
+  onChangeMap = (mapId) => {
+    this.props.setMapId(mapId);
+    this.setState({ show: false });
+  };
 
-  render () {
+  render() {
     return (
-      <div className={'nav-outer ' + (this.state.show ? 'show' : '')}>
-        <div className='nav-button'>
-          <i className='fas fa-caret-right' onClick={this.toggleNav} />
+      <div className={"nav-outer " + (this.state.show ? "show" : "")}>
+        <div className="nav-button">
+          <i className="fas fa-caret-right" onClick={this.toggleNav} />
         </div>
-        <div className={'nav-inner'}>
-          <div className='cate'>1.12</div>
-          <ul className='selector'>
-            <ListBtn changeMap={this.onChangeMap} mapId='90' hasDownload>
+        <div className={"nav-inner"}>
+          <div className="cate">1.12</div>
+          <ul className="selector">
+            <ListBtn changeMap={this.onChangeMap} mapId="90" hasDownload>
               火柴人小品休閒伺服器
             </ListBtn>
-            <ListBtn changeMap={this.onChangeMap} mapId='91' hasDownload>
+            <ListBtn changeMap={this.onChangeMap} mapId="91" hasDownload>
               Project TheArkose
             </ListBtn>
           </ul>
-          <div className='cate'>1.13</div>
-          <ul className='selector'>
-            <ListBtn changeMap={this.onChangeMap} mapId='92'>
+          <div className="cate">1.13</div>
+          <ul className="selector">
+            <ListBtn changeMap={this.onChangeMap} mapId="92">
+              Project TheArkose
+            </ListBtn>
+          </ul>
+          <div className="cate">1.14</div>
+          <ul className="selector">
+            <ListBtn changeMap={this.onChangeMap} mapId="93">
               Project TheArkose
             </ListBtn>
           </ul>
         </div>
       </div>
-    )
+    );
   }
 }
 
 class ListBtn extends React.PureComponent {
   changeMap = () => {
-    this.props.changeMap(this.props.mapId)
-  }
+    this.props.changeMap(this.props.mapId);
+  };
 
   downloadArchive = () => {
-    this.props.downloadArchive(this.props.mapId)
-  }
+    this.props.downloadArchive(this.props.mapId);
+  };
 
-  render () {
+  render() {
     return (
       <li>
-        <div onClick={this.changeMap} className='itemName'>
+        <div onClick={this.changeMap} className="itemName">
           {this.props.children}
         </div>
         {this.props.hasDownload && (
           <a
             onClick={this.downloadArchive}
-            className='download'
+            className="download"
             href={`https://archive.thearkose.com/${this.props.mapId}.zip`}
-            target='_blank'
-            rel='noopener noreferrer'
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <i className='far fa-file-archive ' />
+            <i className="far fa-file-archive " />
           </a>
         )}
       </li>
-    )
+    );
   }
 }
